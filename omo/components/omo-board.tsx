700012582fd0a5610ccae2747e83f4e83bf95441
Hajin Lee <betterthanhajin@icloud.com>
2024-06-23 18:13:20 +0900

    feat: board 상태관리 추가

--- omo/components/omo-board.tsx
+++ omo/components/omo-board.tsx
@@ -1,10 +1,35 @@
 import { useState, useEffect } from "react";
 import Image from "next/image";
-import omoLogo from "@/public/omo-logo.svg";
 import omoArrow from "@/public/omo-arrow.svg";
+import omoArrowWhite from "@/public/arrow-white.svg";
 import omoSearch from "@/public/omo-search.svg";
 
+interface omoTitle {
+  title: string;
+}
+
+interface omoContent {
+  content: string;
+}
+
 export function OmoBoard() {
+  const [hoverIndex, setHoverIndex] = useState<number | null>(null);
+  const [titledata, setTitleData] = useState<omoTitle[]>([]);
+  const [contentData, setContentData] = useState<omoContent[]>([]);
+
+  useEffect(() => {
+    const dummyData: omoTitle[] = Array.from({ length: 10 }, (_, i) => ({
+      title: `props 를 통해 컴포넌트에게 값 전달하기`,
+    }));
+
+    const contentData: omoContent[] = Array.from({ length: 10 }, (_, i) => ({
+      content: `내용...............ㅁ루ㅏㅇ나룽ㄴ미ㅜㄴ이ㅏㅊㅁ나ㅣ + ${i}`,
+    }));
+
+    setTitleData(dummyData);
+    setContentData(contentData);
+  }, []);
+
   return (
     <>
       <div className="flex min-h-40 h-full w-full">
@@ -19,82 +44,50 @@ export function OmoBoard() {
             ></Image>
           </div>
 
-          <ul className="p-2">
-            <li className="flex gap-2 p-2 text-[#645555] hover:bg-[#645555] hover:text-white">
-              <div>props 를 통해 컴포넌트에게 값 전달하기</div>
-              <Image
-                src={omoArrow}
-                alt="omo arrow"
-                width="10"
-                height="10"
-              ></Image>
-            </li>
-            <li className="flex gap-2 p-2 text-[#645555] hover:bg-[#645555] hover:text-white">
-              <div>props 를 통해 컴포넌트에게 값 전달하기</div>
-              <Image
-                src={omoArrow}
-                alt="omo arrow"
-                width="10"
-                height="10"
-              ></Image>
-            </li>
-            <li className="flex gap-2 p-2 text-[#645555] hover:bg-[#645555] hover:text-white">
-              <div>props 를 통해 컴포넌트에게 값 전달하기</div>
-              <Image
-                src={omoArrow}
-                alt="omo arrow"
-                width="10"
-                height="10"
-              ></Image>
-            </li>
-            <li className="flex gap-2 p-2 text-[#645555] hover:bg-[#645555] hover:text-white">
-              <div>props 를 통해 컴포넌트에게 값 전달하기</div>
-              <Image
-                src={omoArrow}
-                alt="omo arrow"
-                width="10"
-                height="10"
-              ></Image>
-            </li>
-            <li className="flex gap-2 p-2 text-[#645555] hover:bg-[#645555] hover:text-white">
-              <div>props 를 통해 컴포넌트에게 값 전달하기</div>
-              <Image
-                src={omoArrow}
-                alt="omo arrow"
-                width="10"
-                height="10"
-              ></Image>
-            </li>
-            <li className="flex gap-2 p-2 text-[#645555] hover:bg-[#645555] hover:text-white">
-              <div>props 를 통해 컴포넌트에게 값 전달하기</div>
-              <Image
-                src={omoArrow}
-                alt="omo arrow"
-                width="10"
-                height="10"
-              ></Image>
-            </li>
-            <li className="flex gap-2 p-2 text-[#645555] hover:bg-[#645555] hover:text-white">
-              <div>props 를 통해 컴포넌트에게 값 전달하기</div>
-              <Image
-                src={omoArrow}
-                alt="omo arrow"
-                width="10"
-                height="10"
-              ></Image>
-            </li>
-            <li className="flex gap-2 p-2 text-[#645555] hover:bg-[#645555] hover:text-white">
-              <div>props 를 통해 컴포넌트에게 값 전달하기</div>
-              <Image
-                src={omoArrow}
-                alt="omo arrow"
-                width="10"
-                height="10"
-              ></Image>
-            </li>
+          <ul>
+            {titledata.map((item, index) => (
+              <li
+                key={index}
+                className="flex gap-2 p-2 text-[#645555] hover:bg-[#645555] hover:text-white"
+                onMouseOver={() => {
+                  setHoverIndex(index);
+                }}
+                onMouseLeave={() => {
+                  setHoverIndex(null);
+                }}
+                onClick={() => {
+                  setHoverIndex(index);
+                }}
+              >
+                <div>{item.title}</div>
+                {hoverIndex === index ? ( // Check if the current item is being hovered
+                  <Image
+                    src={omoArrowWhite}
+                    alt="omo arrow white"
+                    width="10"
+                    height="10"
+                  ></Image>
+                ) : (
+                  <Image
+                    src={omoArrow}
+                    alt="omo arrow"
+                    width="10"
+                    height="10"
+                  ></Image>
+                )}
+              </li>
+            ))}
           </ul>
         </div>
-        <div className="w-1/2 p-8">내용..ascacssacas.</div>
+        {contentData.map((item, index) => (
+          <div
+            key={index}
+            className="w-1/2 p-8"
+            style={{ display: hoverIndex === index ? "block" : "none" }}
+          >
+            {item.content}
+          </div>
+        ))}
       </div>
     </>
   );
