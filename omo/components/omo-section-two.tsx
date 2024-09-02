2711bb00a955616eb22d3a3f8722d77af4f70d1c
Hajin Lee <betterthanhajin@icloud.com>
2024-09-02 09:05:28 +0900

    fix: debug

--- omo/components/omo-section-two.tsx
+++ omo/components/omo-section-two.tsx
@@ -4,7 +4,7 @@ import { useState, useEffect, useRef } from "react";
 
 export default function OmoSectionTwo() {
   const [direction, setDirection] = useState({ x: 1, y: 1 }); // 이동 방향
-  const stepSize = 10; // 한 번에 이동할 픽셀 수
+  const stepSize = 20; // 한 번에 이동할 픽셀 수
   const [position, setPosition] = useState({ x: 0, y: 0 });
   const [isInitialRender, setIsInitialRender] = useState(true);
   const colors = ["#FF6B6B", "#4ECDC4", "#45B7D1", "#F7B731"];
@@ -18,27 +18,40 @@ export default function OmoSectionTwo() {
     }
     const movingCircle = () => {
       if (circleRef.current) {
-        const circleRect = circleRef.current.getBoundingClientRect();
-
-        let newX = position.x + stepSize * direction.x;
-        let newY = position.y + stepSize * direction.y;
+        // const circleRect = circleRef.current.getBoundingClientRect();
+        circleRef.current.clientWidth;
+        console.log(
+          "circleRef.current.clientWidth",
+          circleRef.current.clientWidth
+        );
+        // console.log("circleRect.right", circleRect.right);
+        // console.log("circleRect.left", circleRect.left);
+        let newX = 0;
+        let newY = 0;
         let newDirectionX = direction.x;
         let newDirectionY = direction.y;
-
-        // X 방향 경계 체크
-        if (newX <= circleRect.left || newX >= circleRect.right) {
-          newDirectionX *= -1; // 방향 전환
-          newX = Math.max(circleRect.left, Math.min(newX, circleRect.right));
+        const distance = circleRef.current.clientWidth;
+        // 509 - 259 = 250
+        if (newX <= distance) {
+          newX = position.x + stepSize * direction.x;
+          console.log("newX", newX);
+          setPosition({ x: newX, y: 0 });
+          setDirection({ x: newDirectionX, y: 1 });
         }
+        // } else {
+        //   newX = position.x - stepSize * direction.x;
+        //   console.log("newX22", newX);
+        //   setPosition({ x: newX, y: 0 });
+        //   setDirection({ x: newDirectionX, y: 1 });
+        // }
 
-        // Y 방향 경계 체크
-        if (newY <= circleRect.top || newY >= circleRect.bottom) {
-          newDirectionY *= -1; // 방향 전환
-          newY = Math.max(circleRect.top, Math.min(newY, circleRect.bottom));
-        }
-
-        setPosition({ x: newX, y: newY });
-        setDirection({ x: newDirectionX, y: newDirectionY });
+        // // Y 방향 경계 체크
+        // if (newY <= circleRect.top && newY >= circleRect.bottom) {
+        //   newY = circleRect.top + stepSize;
+        // } else {
+        //   newDirectionY *= -1; // 방향 전환
+        //   newY = circleRect.bottom - stepSize;
+        // }
 
         console.log("Circle moved to:", newX, newY);
       }
