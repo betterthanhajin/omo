aa987708395bfd7986349c03e7e619fb530af92b
Hajin Lee <betterthanhajin@icloud.com>
2024-09-08 02:14:03 +0900

    fix: 수정중

--- omo/components/omo-section-skills.tsx
+++ omo/components/omo-section-skills.tsx
@@ -5,21 +5,46 @@ import { useState, useEffect, useRef, useMemo } from "react";
 export default function OmoSectionsSkills() {
   const [direction, setDirection] = useState({ x: 1 });
   const stepSize = 50;
+  const positionRef = useRef({ x: 0 });
   const [position, setPosition] = useState({ x: 0 });
-  const [isInitialRender, setIsInitialRender] = useState(true);
   const containerRef = useRef<HTMLDivElement>(null);
   const circleRef = useRef<HTMLDivElement>(null);
+  const [, setRender] = useState({});
   const [hoveredIndex, setHoveredIndex] = useState<number | null>(null);
 
   const skills = useMemo(
     () => [
-      { color: "#FFB3BA", name: "NODEJS" },
-      { color: "#e190f3", name: "REACT" },
-      { color: "#FFC0CB", name: "NEXTJS" },
-      { color: "#FF69B4", name: "TYPESCRIPT" },
+      { index: 0, color: "#FFB3BA", name: "NODEJS" },
+      { index: 1, color: "#e190f3", name: "REACT" },
+      { index: 2, color: "#FFC0CB", name: "NEXTJS" },
+      { index: 3, color: "#FF69B4", name: "TYPESCRIPT" },
     ],
     []
   );
+
+  useEffect(() => {
+    const movingCircle = () => {
+      if (containerRef.current) {
+        const containerWidth = containerRef.current.offsetWidth / 2;
+        const circleWidth = 250; // Fixed circle width
+        const maxMove = (containerWidth - circleWidth) / 2;
+        const nextX = stepSize * direction.x;
+
+        if (nextX > containerWidth || nextX < 0) {
+          setDirection((prev) => ({ x: prev.x * -1 }));
+        }
+
+        setPosition((prevPosition) => ({
+          x: position.x + nextX,
+        }));
+
+        console.log("Position updated:", position.x); // 디버깅을 위한 로그
+      }
+    };
+
+    const intervalId = setInterval(movingCircle, 2000);
+    return () => clearInterval(intervalId);
+  }, [direction]);
 
   const SkillCircle = ({
     index,
@@ -30,16 +55,33 @@ export default function OmoSectionsSkills() {
     color: string;
     skill: string;
   }) => {
-    const isRight = index % 2 === 1;
-    const basePosition = isRight ? "0px" : "calc(100% - 250px)";
-
-    return (
+    const isLeft = index % 2 === 0;
+    return isLeft ? (
       <div
         className={`absolute flex justify-center items-center w-[250px] h-[250px] rounded-full transition-all duration-300 ease-in-out`}
         style={{
-          left: `calc(${basePosition} + ${
-            isRight ? position.x : -position.x
-          }px)`,
+          left: position.x,
+          transition: "ease-in-out 0.5s",
+          backgroundColor: color,
+          transform: hoveredIndex === index ? "scale(1.1)" : "scale(1)",
+        }}
+        onMouseEnter={() => setHoveredIndex(index)}
+        onMouseLeave={() => setHoveredIndex(null)}
+      >
+        <span
+          className={`text-[#ffddfb] font-semibold transition-all duration-300 ${
+            hoveredIndex === index ? "text-white" : ""
+          }`}
+        >
+          {skill}
+        </span>
+      </div>
+    ) : (
+      <div
+        className={`absolute flex justify-center items-center w-[250px] h-[250px] rounded-full transition-all duration-300 ease-in-out`}
+        style={{
+          right: position.x,
+          transition: "ease-in-out 0.5s",
           backgroundColor: color,
           transform: hoveredIndex === index ? "scale(1.1)" : "scale(1)",
         }}
@@ -57,33 +99,6 @@ export default function OmoSectionsSkills() {
     );
   };
 
-  useEffect(() => {
-    if (isInitialRender) {
-      setPosition({ x: 0 });
-      setIsInitialRender(false);
-      return;
-    }
-
-    const movingCircle = () => {
-      if (containerRef.current) {
-        const containerWidth = containerRef.current.offsetWidth;
-        const circleWidth = 250; // Fixed circle width
-        const nextX = position.x + stepSize * direction.x;
-
-        if (nextX > 500 || nextX < 0) {
-          setDirection((prev) => ({ x: prev.x * -1 }));
-        }
-
-        setPosition(() => ({
-          x: nextX,
-        }));
-      }
-    };
-
-    const intervalId = setInterval(movingCircle, 2000);
-    return () => clearInterval(intervalId);
-  }, [position, direction, isInitialRender]);
-
   return (
     <section>
       <div className="w-full h-[500px] relative" ref={containerRef}>
@@ -91,7 +106,7 @@ export default function OmoSectionsSkills() {
           {skills.slice(0, 2).map((skill, index) => (
             <SkillCircle
               key={index}
-              index={index}
+              index={skill.index}
               color={skill.color}
               skill={skill.name}
             />
@@ -110,8 +125,8 @@ export default function OmoSectionsSkills() {
         <div className="flex relative h-[250px]">
           {skills.slice(2, 4).map((skill, index) => (
             <SkillCircle
-              key={index + 2}
-              index={index + 2}
+              key={index}
+              index={skill.index}
               color={skill.color}
               skill={skill.name}
             />
