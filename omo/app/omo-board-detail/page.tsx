6abc5476a9b201dc270d897c1e27b61c6b8b6d02
Hajin Lee <betterthanhajin@icloud.com>
2024-12-14 15:37:21 +0900

    fix: 수정중

--- omo/app/omo-board-detail/page.tsx
+++ omo/app/omo-board-detail/page.tsx
@@ -3,35 +3,49 @@ import React, { useState, useEffect } from "react";
 import { Sticker } from "lucide-react";
 import { useSearchParams } from "next/navigation";
 
-interface omoContent {
+interface OmoContent {
+  index: string;
   content: string;
 }
 
-export default function OmoBoardDetail() {
-  const [contentData, setContentData] = useState<omoContent[]>([]);
+function OmoBoardDetailContent() {
+  const [contentData, setContentData] = useState<OmoContent[]>([]);
+  const [selectedIndex, setSelectedIndex] = useState<string | null>(null);
   const searchParams = useSearchParams();
-  const selectedIndex = searchParams.get("index");
 
   useEffect(() => {
-    const contentData: omoContent[] = Array.from({ length: 10 }, (_, i) => ({
+    setSelectedIndex(searchParams.get("index") ?? "");
+    const contentData: OmoContent[] = Array.from({ length: 10 }, (_, i) => ({
+      index: String(i),
       content: `내용...............+ ${i}`,
     }));
     setContentData(contentData);
-  }, []);
-
-  const selectedContent =
-    selectedIndex !== null ? contentData[parseInt(selectedIndex)] : null;
+  }, [searchParams]);
 
   return (
     <div className="bg-[#ff8a8a] rounded-md w-full">
-      {selectedContent ? (
-        <div className="w-full p-8">{selectedContent.content}</div>
-      ) : (
+      {contentData.map(
+        (content) =>
+          content.index === selectedIndex && (
+            <div key={content.index} className="w-full p-8">
+              {content.content}
+            </div>
+          )
+      )}
+      {contentData.length === 0 && (
         <div className="p-8 flex flex-col justify-center items-center w-full h-full">
           <Sticker size={50} />
           <p className="text-center">블로그 글을 선택해주세요!</p>
         </div>
       )}
     </div>
+  );
+}
+
+export default function OmoBoardDetail() {
+  return (
+    <React.Suspense fallback={<div>Loading...</div>}>
+      <OmoBoardDetailContent />
+    </React.Suspense>
   );
 }
